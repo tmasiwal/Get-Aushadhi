a603f16fb7fdb3358c6e9c5de1796bea09337092
tmasiwal <tanujmasiwal@gmail.com>
2023-06-19 04:09:10 +0530

    done

--- get-aushadhi/src/Component/Allroutes.jsx
+++ get-aushadhi/src/Component/Allroutes.jsx
@@ -3,12 +3,16 @@ import {Route,Routes} from 'react-router-dom'
 import Home from '../page/Home'
 import Calender from '../page/Calender'
 import Profile from '../page/Profile'
+import Product from '../page/Product'
+import Cart from '../page/Cart'
 const Allroutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/calender' element={<Calender/>}/>
       <Route path='/profile' element={<Profile/>}/>
+      <Route path='/medicineData/:id' element={<Product/>}/>
+      <Route path='/cart' element={<Cart/>}/>
     </Routes>
   )
 }
--- get-aushadhi/src/page/card.jsx
+++ get-aushadhi/src/page/card.jsx
@@ -1,29 +1,55 @@
 import React,{useState} from 'react'
-import { Card, CardHeader, CardBody, Collapse,Button,Stack,Heading,Image,Text,Divider,CardFooter,ButtonGroup } from '@chakra-ui/react'
-const Cards = ({image,description,price,name
-}) => {
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
+} from '@chakra-ui/react'
 
+import { Card, CardHeader, CardBody, Collapse,Button,Stack,Heading,Image,Text,Divider,CardFooter,ButtonGroup } from '@chakra-ui/react'
+import { Link, Navigate } from 'react-router-dom'
+const Cards = (sdata) => {
+console.log(sdata,"g")
     const [show, setShow] = useState(false)
     const handleToggle = () => setShow(!show)
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const ldata= JSON.parse(localStorage.getItem('medicineData'))||[]
+    const hendleCart=(id)=>{
+ let check=ldata.find((el)=>{return el.id==id})
+ if(check){
+onOpen()
+ }
+else{
+  let data1={...sdata,quantity:1}
+  ldata.push(data1)
+localStorage.setItem("medicineData",JSON.stringify(ldata))}
+
+    }
+
   return (
     <div>
       <Card maxW='sm'>
   <CardBody>
-    <Image
-      src={image[0]}
+ <Link to={`/medicineData/${sdata.id}`}>   <Image
+      src={sdata.image[0]}
       alt=''
       borderRadius='lg'
       boxSize={"250px"}
-    />
+      
+    /></Link>
     <Stack mt='6' spacing='3'>
-      <Heading size='md'>{name}</Heading>
+      <Heading size='md'>{sdata.name}</Heading>
       <Collapse startingHeight={20} in={show}>
-        {description}</Collapse>
+        {sdata.description}</Collapse>
       <Button size='sm' onClick={handleToggle} mt='1rem'>
         Show {show ? 'Less' : 'More'}
       </Button>
       <Text color='blue.600' fontSize='2xl'>
-        ₹ {price}
+        ₹ {sdata.price}
       </Text>
     </Stack>
   </CardBody>
@@ -33,12 +59,29 @@ const Cards = ({image,description,price,name
       <Button variant='solid' colorScheme='blue'>
         Buy now
       </Button>
-      <Button variant='ghost' colorScheme='blue'>
+      <Button variant='ghost' colorScheme='blue' onClick={()=>{hendleCart(sdata.id)}}> 
         Add to cart
       </Button>
     </ButtonGroup>
   </CardFooter>
 </Card>
+<Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+           <Text>Product is already in cart</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+           
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   )
 }
